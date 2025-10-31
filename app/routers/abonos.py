6773367fbef6578fd625bc248e7dbfc86df13254
nicolas <nicolasgpla@gmail.com>
2025-10-31 16:35:18 -0500

    feat: (Payments): add JWT and multi-tenant validation for payment list by invoice ID

--- app/routers/abonos.py
+++ app/routers/abonos.py
@@ -44,5 +44,11 @@ def read_abono(abono_id: int, db: Session = Depends(get_db), empresa: Empresa = 
     return abono
 
 @router.get("/listado/{factura_id}", response_model=list[AbonoResponse])
-def get_abonos_por_factura(factura_id: int, db: Session = Depends(get_db)):
+def get_abonos_por_factura(factura_id: int, db: Session = Depends(get_db), empresa: Empresa = Depends(get_current_empresa)):
+    factura_data = db.query(Factura).filter(Factura.id == factura_id).first()
+    if not factura_data:
+        raise HTTPException(status_code=404, detail="Factura no encontrada o no pertenece a la empresa actual.")
+    client_data = db.query(Cliente).filter(Cliente.id == factura_data.cliente_id).first()
+    if client_data.empresa_id != empresa.id:
+        raise HTTPException(status_code=404, detail="Factura no encontrada o no pertenece a la empresa actual.")
     return obtener_abonos_por_factura(db, factura_id)
