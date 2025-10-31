8793e1f9244f5e83c3849d8c2ed3c2607a94503b
nicolas <nicolasgpla@gmail.com>
2025-10-31 16:03:35 -0500

    feat: (Payments): add JWT validation for payments with multi-tenant support

--- app/routers/abonos.py
+++ app/routers/abonos.py
@@ -4,11 +4,27 @@ from app.db.session import get_db
 from app.schemas.abono import AbonoCreate, AbonoResponse
 from app.services.abono_service import crear_abono, obtener_abono
 from app.services.abono_service import obtener_abonos_por_factura
+from app.core.deps import get_current_empresa
+from app.models.cliente import Cliente
+from app.models.empresa import Empresa
+from app.models.factura import Factura
 
 router = APIRouter()
 
 @router.post("/", response_model=AbonoResponse)
-def create_abono(abono: AbonoCreate, db: Session = Depends(get_db)):
+def create_abono(abono: AbonoCreate, db: Session = Depends(get_db), empresa: Empresa = Depends(get_current_empresa)):
+    client_data = db.query(Cliente).filter(Cliente.id == abono.cliente_id).first()
+    if client_data.empresa_id != empresa.id:
+        raise HTTPException(
+            status_code=400,
+            detail="El cliente no existe o no pertenece a la empresa actual."
+        )
+    factura_data = db.query(Factura).filter(Factura.id == abono.factura_id).first()
+    if factura_data.cliente_id != abono.cliente_id:
+        raise HTTPException(
+            status_code=400,
+            detail="La factura no existe o no pertenece al cliente proporcionado."
+        )
     return crear_abono(db, abono)
 
 @router.get("/{abono_id}", response_model=AbonoResponse)
